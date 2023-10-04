376f0bc2b784caca48762b6eeba99fa04e26bfce
Jrgnrq <118330583+Jrgnrq@users.noreply.github.com>
2023-10-04 13:51:29 -0600

    exercise 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(stringReapet, n) {
+    if (n < 0){
+        return "ERROR";
+    }
+    else{
+        let stringRepeated = stringReapet.repeat(n);
+        return stringRepeated; 
+    }
+}
+let stringReapet = "hey"; 
+// const number = 3; //
+// const number = 1; //
+// const number = 0; //
+// const number = -1; //
+// const numberRandom = Math.floor(Math.random() * 1000); //
 // Do not edit below this line
 module.exports = repeatString;
