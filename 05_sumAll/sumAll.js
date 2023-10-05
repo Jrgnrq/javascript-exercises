efa830ea44835b13434c80ee9b5314b7bb1651db
Jrgnrq <118330583+Jrgnrq@users.noreply.github.com>
2023-10-05 17:24:43 -0600

    Exercise 05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let argumentsSort = [a, b].sort(function(a, b) {
+        return a - b
+    })
+    let sumaAB = 0
+    if ((isNaN(argumentsSort[0]) || isNaN(argumentsSort[1]))||((argumentsSort[0] < 0 ) || (argumentsSort[1] < 0)) || (typeof argumentsSort[0] === 'string')||(typeof argumentsSort[1] === 'string')){  
+        sumaAB = 'ERROR'
 
+    } 
+    else{
+        for (let i = argumentsSort[0]; i <= argumentsSort[1]; i++) {
+            sumaAB += i  
+    }    
+    }
+    return sumaAB;
 };
 
 // Do not edit below this line
