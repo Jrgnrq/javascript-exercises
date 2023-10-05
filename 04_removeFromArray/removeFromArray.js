9a930977693abcfa412fe32b312ee52e04b03a78
Jrgnrq <118330583+Jrgnrq@users.noreply.github.com>
2023-10-04 21:08:31 -0600

    Exercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
 const removeFromArray = function() {
-
-};
+    let argument = Array.from(arguments);
+    let originalArray = argument[0];
+    let elementsToDelete = argument.filter(item => item !== originalArray);
+    let newArray = originalArray.filter(item => !elementsToDelete.includes(item));
+    return newArray
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
