f2f0cc3c1954db0a1e357696070369e4909d7107
Jrgnrq <118330583+Jrgnrq@users.noreply.github.com>
2023-10-05 18:38:24 -0600

    Exercise 07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) { 
+    let tempToC =(tempF - 32)*(5/9) ;
+    return parseFloat(tempToC.toFixed(1)) ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  
+  let tempToF = (tempC * (9/5)) + 32 ;
+  return parseFloat(tempToF.toFixed(1)) ;
 };
 
 // Do not edit below this line
