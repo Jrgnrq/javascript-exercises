a76618f7d16d25771aadf3c1fc971b067969c697
Jrgnrq <118330583+Jrgnrq@users.noreply.github.com>
2023-10-30 18:33:57 -0600

    Exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    
+    cleanString = string.replace(/[ ,!.]/g, '');
+    lowerString = cleanString.toLowerCase();
+    reversedString = lowerString.split('').reverse().join('');
+    if (reversedString == lowerString){
+        return true;
+    }
+    else{ 
+        return false;
+    }
 };
 
 // Do not edit below this line
